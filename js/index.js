a3af78cd8b3eb4e3febace60d05acacfe67920e0
dtungmissemvcb <145363606+dtmissemvcb@users.noreply.github.com>
2025-02-07 23:31:51 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'to co dieu nay muon moi'.split('')
+const text = 'to co dieu nay muon noii'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
